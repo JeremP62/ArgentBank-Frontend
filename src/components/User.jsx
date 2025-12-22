786d644cd382c8fbb1b85f80d47c9ae77bf7fc1e
jerem p <palette.jeremie@live.fr>
2025-12-22 20:39:58 +0100

    add components scss

---
+++ src/components/User.jsx
@@ -0,0 +1,10 @@
+function User({ firstname, lastname }) {
+  return (
+    <div className="header">
+      <h1>Welcome back<br />{firstname} {lastname}!</h1>
+      <button className="edit-button">Edit Name</button>
+    </div>
+  );
+}
+
+export default User;

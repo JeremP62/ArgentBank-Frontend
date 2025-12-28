856c4db42516963ce71f587a2b9fd1058bd8f83b
jerem p <palette.jeremie@live.fr>
2025-12-28 12:27:02 +0100

    Login+cssLogin

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,18 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Home from './pages/Home';
+import Login from './pages/Login';
+
 
 function App() {
   return (
     <Router>
       <Header />
-
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/login" element={<div>Sign In</div>} />
+        <Route path="/login" element={<Login />} />
         <Route path="/profile" element={<div>User</div>} />
       </Routes>
-
       <footer className='footer'>
         <p>Copyright 2020 Argent Bank</p>
       </footer>
@@ -20,4 +20,4 @@ function App() {
   )
 }
 
-export default App
+export default App;
